8f76658efa531a6d116cdbf6271ef4c25fe340d3
AleksandrUshenin <sasha0625@mail.ru>
2022-07-01 21:58:45 +0300

    Выполнено дз №2

---
+++ Lesson_2/Account.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    /// <summary>
+    /// типы аккаунтов юзера
+    /// </summary>
+    [Flags]
+    enum TypeUser
+    {
+        User = 0b_0001,
+        ProUser = 0b_0010,
+        Admin = 0b_1111
+    }
+    /// <summary>
+    /// класс аккаунт
+    /// </summary>
+    internal class Account
+    {
+        /// <summary>
+        /// вкл/выкл уведомление 
+        /// </summary>
+        public bool Messege { get; set; }
+        /// <summary>
+        /// статическая переменная для генерации id
+        /// </summary>
+        static int SID;
+        public int ID { get; private set; }
+        public decimal Balance { get; private set; }
+        public TypeUser Type { get; private set; }
+
+        /// <summary>
+        /// конструктор без параметра 
+        /// </summary>
+        public Account()
+        {
+            ID = GenId();
+            Type = TypeUser.User;
+            Messege = true;
+        }
+        /// <summary>
+        /// конструктор с балансом аккаунта
+        /// </summary>
+        /// <param name="bal"></param>
+        public Account(decimal bal)
+        {
+            ID = GenId();
+            Balance = bal;
+            Messege = true;
+        }
+        /// <summary>
+        /// конструктор с типом аккаунта
+        /// </summary>
+        /// <param name="type">тип аккаунта</param>
+        public Account(TypeUser type)
+        {
+            ID = GenId();
+            Type = type;
+            Messege = true;
+        }
+        /// <summary>
+        /// полный конструктор
+        /// </summary>
+        /// <param name="bal">баланс</param>
+        /// <param name="type">тип аккаунта</param>
+        public Account(decimal bal, TypeUser type)
+        {
+            ID = GenId();
+            Balance = bal;
+            Type = type;
+            Messege = true;
+        }
+        /// <summary>
+        /// генерация id
+        /// </summary>
+        /// <returns></returns>
+        int GenId()
+        {
+            return ++SID;
+        }
+        /// <summary>
+        /// положить деньги на счет
+        /// </summary>
+        /// <param name="put">сумма</param>
+        public void PutMoney(decimal put)
+        {
+            Balance += put;
+            PrintMessage("Поступило: " + put + "$");
+            PrintMessage("Текущий бaланс: " + Balance + "$");
+        }
+        /// <summary>
+        /// снять с счета
+        /// </summary>
+        /// <param name="getm">сумма</param>
+        public void TakeMoney(decimal getm)
+        {
+            if (getm <= Balance)
+            {
+                Balance -= getm;
+                PrintMessage("Снято: " + getm + "$");
+                PrintMessage("Текущий бaланс: " + Balance + "$");
+            }
+        }
+        void PrintMessage(string mess)
+        {
+            if (Messege)
+            {
+                Console.WriteLine(mess);
+            }
+        }
+    }
+}
---
+++ Lesson_2/TEST1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    internal class TEST1
+    {
+        List<Account> accounts;
+        public TEST1()
+        {
+            accounts = new List<Account>();
+            accounts.Add(new Account());
+            accounts.Add(new Account(12345));
+            accounts.Add(new Account(TypeUser.Admin));
+            accounts.Add(new Account(800, TypeUser.ProUser));
+        }
+        public void RUN()
+        {
+            var tests = new object[] { Test1(), Test2(), Test3(), Test4(), Test5() };
+            for (int i = 0; i < tests.Length; i++)
+            {
+                var res = tests[i];
+                PrintResult((bool)res);
+            }
+        }
+        void PrintResult(bool resulr)
+        {
+            if (resulr)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Test Complit!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Test Failed!");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        bool Test1()
+        {
+            if (accounts[0].ID == 1 && accounts[0].Balance == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        bool Test2()
+        {
+            accounts[0].PutMoney(1000);
+            if (accounts[0].Balance == 1000)
+            {
+                accounts[0].TakeMoney(500);
+                if (accounts[0].Balance == 500)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        bool Test3()
+        {
+            if (accounts[1].ID == 2 && accounts[1].Balance == 12345)
+            {
+                return true;
+            }
+            return false;
+        }
+        bool Test4()
+        {
+            if (accounts[2].ID == 3 && accounts[2].Type == TypeUser.Admin)
+            {
+                return true;
+            }
+            return false;
+        }
+        bool Test5()
+        {
+            if (accounts[3].Balance == 800 && accounts[3].Type == TypeUser.ProUser && accounts[3].ID == 4)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
