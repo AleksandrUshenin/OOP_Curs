6c4134cbdd013558a9c6bb3657e2871c083bb118
AleksandrUshenin <sasha0625@mail.ru>
2022-07-05 22:07:26 +0300

    Выполнено дз 3, сделана сериализация бинарная,  чтение из файла и сохранение в файл

---
+++ Lesson_3/SerialesUsers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Lesson_3
+{
+    internal class SerialesUsers
+    {
+        private List<Information> inform;
+        private string dir;
+        public List<Information> Datas { get { return inform; } }
+
+        public SerialesUsers()
+        {
+            inform = new List<Information>();
+            dir = Path.Combine(GetDirHome(), "Data_serealrs");
+            HaveDir(dir);
+            dir = Path.Combine(dir, "User_Data.txt");
+        }
+
+        public void AddInfo(Information info)
+        {
+            inform.Add(info);
+        }
+        public void Save()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (FileStream fs = new FileStream(dir, FileMode.OpenOrCreate))
+            {
+                bf.Serialize(fs, inform);
+            }
+        }
+        public void Load()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (FileStream fs = new FileStream(dir, FileMode.OpenOrCreate))
+            {
+                inform = (List<Information>)bf.Deserialize(fs);
+            }
+        }
+        public void SetDir(string dir)
+        {
+            this.dir = dir;
+        }
+        public List<Information> GetList()
+        {
+            if (inform != null)
+            {
+                return inform;
+            }
+            return null;
+        }
+
+        private string GetDirHome()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+        private void HaveDir(string dir)
+        {
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+        }
+        private void HaveFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName);
+            }
+        }
+    }
+}
---
+++ Lesson_3/TEST2.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3
+{
+    internal class TEST2 : Admin
+    {
+        private string Password = "qwerty";
+        private Bank bankTest;
+        private List<Account> listAcc;
+
+        public TEST2()
+        {
+            bankTest = new Bank();
+        }
+
+        public void Enter()
+        {
+            Console.Write("Введите пароль: ");
+            if (Console.ReadLine() == Password)
+            {
+                bankTest.AddNewAcc(TypeUser.Admin);
+                bankTest.AddNewAcc();
+                RUN();
+            }
+        }
+        private void RUN()
+        {
+            bankTest.Enter(1, Password);
+            listAcc = bankTest.Testadmin(NTestN);
+
+            var tests = new bool[] { Test1(), Test2(), Test3(), Test4(), Test5() };
+            for (int i = 0; i < tests.Length; i++)
+            {
+                var res = tests[i];
+                PrintResult(res);
+            }
+        }
+        private void PrintResult(bool resulr)
+        {
+            if (resulr)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Test Complit!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Test Failed!");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        private bool Test1()
+        {
+            return listAcc[0].ID == 1 && listAcc[0].Balance == 0;
+        }
+        private bool Test2()
+        {
+            listAcc[0].PutMoney(1000);
+            if (listAcc[0].Balance == 1000)
+            {
+                listAcc[0].GetMoney(500);
+                if (listAcc[0].Balance == 500)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool Test3()
+        {
+            List<Account> AT = new List<Account>();
+            AT.Add(new Account("test", "test", "test"));
+            AT.Add(new Account(12345, "test", "test", "test"));
+            AT.Add(new Account(TypeUser.ProUser, "test", "test", "test"));
+            AT.Add(new Account(12345, TypeUser.ProUser, "test", "test", "test"));
+            for (int i = 0; i < 3; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        if (!(AT[0].FirstName == "test" && AT[0].SecondName == "test" && AT[0].provPass("test")))
+                        {
+                            return false;
+                        }
+                        break;
+                    case 1:
+                        if (!(AT[1].Balance == 12345 && AT[1].FirstName == "test" && AT[1].SecondName == "test" && AT[1].provPass("test")))
+                        {
+                            return false;
+                        }
+                        break;
+                    case 2:
+                        if (!(AT[2].Type == TypeUser.ProUser && AT[2].FirstName == "test" && AT[2].SecondName == "test" && AT[2].provPass("test")))
+                        {
+                            return false;
+                        }
+                        break;
+                    case 3:
+                        if (!(AT[3].Balance == 12345 && AT[3].Type == TypeUser.ProUser && AT[3].FirstName == "test" && AT[3].SecondName == "test" && AT[3].provPass("test")))
+                        {
+                            return false;
+                        }
+                        break;
+                }
+            }
+            return true;
+        }
+        private bool Test4()
+        {
+            return listAcc[0].Type == TypeUser.Admin && listAcc[1].ID == 2 && listAcc[1].Type == TypeUser.User;
+        }
+        private bool Test5()
+        {
+            bankTest.PutMoney();
+            bankTest.GetMoney();
+            bankTest.Perevod(2);
+            bankTest.Perevod(1);
+            Console.Write("Все ОК?: ");
+            return Console.ReadLine() == "y";
+        }
+    }
+}
