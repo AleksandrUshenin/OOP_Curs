25655388ac24c05debc39a9f3734666d13919cf4
AleksandrUshenin <sasha0625@mail.ru>
2022-07-15 22:39:19 +0300

    Выполнено дз 6

---
+++ Lesson_6/Figure.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    enum Colors
+    {
+        red = 1,
+        black,
+        blue,
+        green,
+        yellow,
+        non_color
+    }
+
+    internal class Figure
+    {
+        protected bool Visible;
+        private Colors Color_Figure;
+
+        public Figure(bool visible, string color)
+        {
+            Visible = visible;
+            ChengeColor(color);
+        }
+
+        public virtual void MoveGorisontal(double x) { }
+        public virtual void MoveVertical(double y) { }
+
+        /// <summary>
+        /// input red, black, blue, green, yellow
+        /// </summary>
+        /// <param name="color"></param>
+        public bool ChengeColor(string color)
+        {
+            switch (color)
+            {
+                case "red":
+                    Color_Figure = Colors.red;
+                    return true;
+                case "black":
+                    Color_Figure = Colors.black;
+                    return true;
+                case "blue":
+                    Color_Figure = Colors.blue;
+                    return true;
+                case "green":
+                    Color_Figure = Colors.green;
+                    return true;
+                case "yellow":
+                    Color_Figure = Colors.yellow;
+                    return true;
+                default:
+                    Color_Figure = Colors.non_color;
+                    return false;
+            }
+        }
+        protected void Status()
+        {
+            Console.WriteLine("Visible: {0}  Color:{1}", Visible, Color_Figure);
+        }
+    }
+}
---
+++ Lesson_6/FigureBilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    internal class FigureBilder
+    {
+        /// <summary>
+        /// создать круг 
+        /// </summary>
+        /// <param name="position_x"></param>
+        /// <param name="position_y"></param>
+        /// <param name="radius"></param>
+        /// <param name="color"></param>
+        /// <param name="visible"></param>
+        /// <returns></returns>
+        public static Circle CreateCircle(double position_x, double position_y, double radius, string color, bool visible)
+        {
+            return new Circle(position_x, position_y, visible, color, radius);
+        }
+
+        /// <summary>
+        /// создать прямоугольник 
+        /// </summary>
+        /// <param name="position_x"></param>
+        /// <param name="position_y"></param>
+        /// <param name="length"></param>
+        /// <param name="width"></param>
+        /// <param name="color"></param>
+        /// <param name="visible"></param>
+        /// <returns></returns>
+        public static Rectangle CreateRactangle(double position_x, double position_y, double length, double width,
+            string color, bool visible)
+        {
+            return new Rectangle(position_x, position_y, visible, color, length, width);
+        }
+    }
+}
